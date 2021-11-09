e9d7be7eb6caa8e88a7fdc5c1c38e8affebc8c75
Nicolas Degen <ndegen@me.com>
2021-11-09 15:54:28 +0100

    override output filename and dir

--- Sources/Draw/DrawTask.swift
+++ Sources/Draw/DrawTask.swift
@@ -8,35 +8,48 @@ struct DrawTask: Codable {
   
   struct Resolution: Codable {
     var pixels: Float
-    var scales: [Int]
+    var scales: [Int]?
     var resolutionSuffix: Bool?
-    
-    func commands(location: URL, inputFile: String, outputDir: String, outputFilename: String, forceOverride: Bool) -> [String] {
-      scales.compactMap { scale in
-        let scaleExtension = scale != 1 ? "@\(scale)x" : ""
-        
-        var destination: URL = (outputDir.first == ".") ? location.appendingPathComponent(outputDir) : URL(fileURLWithPath: outputDir)
-        destination.appendPathComponent(outputFilename +
-                                          ((resolutionSuffix ?? true) ? "\(pixels.cleanString)" : "") +
-                                          scaleExtension)
-        destination.appendPathExtension("png")
-        
-        let inputURL = (inputFile.first == ".") ? location.appendingPathComponent(inputFile) : URL(fileURLWithPath: inputFile)
-        
-        if forceOverride || !FileManager.default.fileExists(atPath: destination.path)  {
-          return "file-open:\(inputURL.path); export-filename:\(destination.path); export-width:\(pixels * Float(scale)); export-do"
-        } else {
-          return nil
-        }
+    var outputDirOverride: String?
+    var outputFilenameOverride: String?
+  }
+}
+
+extension DrawTask.Resolution {
+  func commands(location: URL, inputFile: String, outputDir: String,
+                outputFilename: String, forceOverride: Bool) -> [String] {
+    let actualOutputDir = outputDirOverride ?? outputDir
+    let actualOutputFilename = outputFilenameOverride ?? outputFilename
+    let actualScales = scales ?? [1]
+    return actualScales.compactMap { scale in
+      let scaleExtension = scale != 1 ? "@\(scale)x" : ""
+      
+      var destination: URL = (actualOutputDir.first == ".") ? location.appendingPathComponent(actualOutputDir) : URL(fileURLWithPath: actualOutputDir)
+      var filename = actualOutputFilename
+      if resolutionSuffix ?? (outputFilenameOverride == nil) {
+        filename += "\(pixels.cleanString)"
+      }
+      filename += scaleExtension
+      
+      destination.appendPathComponent(filename)
+      
+      destination.appendPathExtension("png")
+      
+      let inputURL = (inputFile.first == ".") ? location.appendingPathComponent(inputFile) : URL(fileURLWithPath: inputFile)
+      
+      if forceOverride || !FileManager.default.fileExists(atPath: destination.path)  {
+        return "file-open:\(inputURL.path); export-filename:\(destination.path); export-width:\(pixels * Float(scale)); export-do"
+      } else {
+        return nil
       }
     }
   }
 }
 
 extension Float {
-    var cleanString: String {
-       return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(format: "%.1f", self)
-    }
+  var cleanString: String {
+    return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(format: "%.1f", self)
+  }
 }
 
 extension DrawTask {
@@ -45,59 +58,59 @@ extension DrawTask {
   
   
   static var test =
-    [
-      DrawTask(
-        inputFile:"./Resources/Icons/AppIcon.svg",
-        outputDir:"./Resources/Assets.xcassets/AppIcon.appiconset",
-        outputFilename:"AppIcon",
-        scaledResolutions: [
-          DrawTask.Resolution(pixels: 20, scales: [1,2,3]),
-          DrawTask.Resolution(pixels: 29, scales: [1,2,3]),
-          DrawTask.Resolution(pixels: 40, scales: [1,2,3]),
-          DrawTask.Resolution(pixels: 60, scales: [2,3]),
-          DrawTask.Resolution(pixels: 76, scales: [1,2]),
-          DrawTask.Resolution(pixels: 83.5, scales: [2]),
-          DrawTask.Resolution(pixels: 1024, scales: [1]),
-        ]
-      ),
-      DrawTask(
-        inputFile:"./Resources/Icons/CrosshairRed.svg",
-        outputDir:"./Resources/Assets.xcassets/CrosshairRed.appiconset",
-        outputFilename:"CrosshairRed",
-        scaledResolutions: [DrawTask.Resolution(pixels: 100, scales: [1,2,3])]
-      ),
-      DrawTask(
-        inputFile: "./Resources/Icons/RainbowAppIcon.svg",
-        outputDir: ".Resources/AlternateAppIcons",
-        outputFilename: "RainbowAppIcon",
-        scaledResolutions: [DrawTask.Resolution(pixels: 60, scales: [2,3])]
-      ),
-      DrawTask(
-        inputFile: "./Resources/Icons/RedWhiteAppIcon.svg",
-        outputDir: ".Resources/AlternateAppIcons",
-        outputFilename: "RedWhiteAppIcon",
-        scaledResolutions: [DrawTask.Resolution(pixels: 60, scales: [2,3])]
-      ),
-      DrawTask(
-        inputFile: "./Resources/Icons/BlackWhiteAppIcon.svg",
-        outputDir: ".Resources/AlternateAppIcons",
-        outputFilename: "BlackWhiteAppIcon",
-        scaledResolutions: [DrawTask.Resolution(pixels: 60, scales: [2,3])]
-      ),
-      DrawTask(
-        inputFile: "./Resources/Icons/WhiteBlackAppIcon.svg",
-        outputDir: ".Resources/AlternateAppIcons",
-        outputFilename: "WhiteBlackAppIcon",
-        scaledResolutions: [DrawTask.Resolution(pixels: 60, scales: [2,3])]
-      ),
-      DrawTask(
-        inputFile: "./Resources/Icons/TableIconDeveloper.svg",
-        outputDir: ".Resources/Assets.xcassets/TableIconDeveloper.imageset",
-        outputFilename: "TableIconDeveloper",
-        scaledResolutions: [DrawTask.Resolution(pixels: 30, scales: [1, 2, 3])]
-      ),
-    ]
-
+  [
+    DrawTask(
+      inputFile:"./Resources/Icons/AppIcon.svg",
+      outputDir:"./Resources/Assets.xcassets/AppIcon.appiconset",
+      outputFilename:"AppIcon",
+      scaledResolutions: [
+        DrawTask.Resolution(pixels: 20, scales: [1,2,3]),
+        DrawTask.Resolution(pixels: 29, scales: [1,2,3]),
+        DrawTask.Resolution(pixels: 40, scales: [1,2,3]),
+        DrawTask.Resolution(pixels: 60, scales: [2,3]),
+        DrawTask.Resolution(pixels: 76, scales: [1,2]),
+        DrawTask.Resolution(pixels: 83.5, scales: [2]),
+        DrawTask.Resolution(pixels: 1024, scales: [1]),
+      ]
+    ),
+    DrawTask(
+      inputFile:"./Resources/Icons/CrosshairRed.svg",
+      outputDir:"./Resources/Assets.xcassets/CrosshairRed.appiconset",
+      outputFilename:"CrosshairRed",
+      scaledResolutions: [DrawTask.Resolution(pixels: 100, scales: [1,2,3])]
+    ),
+    DrawTask(
+      inputFile: "./Resources/Icons/RainbowAppIcon.svg",
+      outputDir: ".Resources/AlternateAppIcons",
+      outputFilename: "RainbowAppIcon",
+      scaledResolutions: [DrawTask.Resolution(pixels: 60, scales: [2,3])]
+    ),
+    DrawTask(
+      inputFile: "./Resources/Icons/RedWhiteAppIcon.svg",
+      outputDir: ".Resources/AlternateAppIcons",
+      outputFilename: "RedWhiteAppIcon",
+      scaledResolutions: [DrawTask.Resolution(pixels: 60, scales: [2,3])]
+    ),
+    DrawTask(
+      inputFile: "./Resources/Icons/BlackWhiteAppIcon.svg",
+      outputDir: ".Resources/AlternateAppIcons",
+      outputFilename: "BlackWhiteAppIcon",
+      scaledResolutions: [DrawTask.Resolution(pixels: 60, scales: [2,3])]
+    ),
+    DrawTask(
+      inputFile: "./Resources/Icons/WhiteBlackAppIcon.svg",
+      outputDir: ".Resources/AlternateAppIcons",
+      outputFilename: "WhiteBlackAppIcon",
+      scaledResolutions: [DrawTask.Resolution(pixels: 60, scales: [2,3])]
+    ),
+    DrawTask(
+      inputFile: "./Resources/Icons/TableIconDeveloper.svg",
+      outputDir: ".Resources/Assets.xcassets/TableIconDeveloper.imageset",
+      outputFilename: "TableIconDeveloper",
+      scaledResolutions: [DrawTask.Resolution(pixels: 30, scales: [1, 2, 3])]
+    ),
+  ]
+  
   
   func inkscapeCommands(forceOverride: Bool = false, relativeTo location: URL) -> [String] {
     scaledResolutions.flatMap { (resolution: Resolution) in
